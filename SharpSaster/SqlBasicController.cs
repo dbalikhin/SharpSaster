cd9aaecb5dadc8e8e2cd0ae31b06a3e859e231ed
Dmitry <dmodeus@gmail.com>
2022-11-14 10:14:53 -0500

    Sql Controllers Added

---
+++ SharpSaster/SqlBasicController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SharpSaster.Data;
+
+namespace SharpSaster
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlBasicController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SqlBasicController(DataContext context)
+        {
+            _context= context; 
+        }
+
+        public IActionResult EfCoreInjectionSafe(string user)
+        {
+            // Raw Sql Same - one line
+            // SAFE
+            _context.Accounts
+                .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login={0}", user);
+
+            // ExecuteSqlRaw - one line
+            // SAFE
+            _context.Database.ExecuteSqlRaw("SELECT * FROM ACCOUNTS WHERE login={0}", user);
+
+            // Raw SQL multi line
+            // SAFE
+            _context.Accounts
+                .FromSqlRaw(
+                    "SELECT * FROM ACCOUNTS WHERE login={0}",
+                    user
+                );
+
+            // ExecuteSqlRaw - multi line
+            // SAFE
+            _context.Database.ExecuteSqlRaw(
+                "SELECT * FROM ACCOUNTS WHERE login={0}",
+                user
+            );
+
+            // Interpolated Sql / ExecuteSqlInterpolated with an interpolated sql str
+            // SAFE
+            _context.Accounts
+                .FromSqlInterpolated($"SELECT * FROM ACCOUNTS WHERE login = '{user}'");
+            // SAFE
+            _context.Database.ExecuteSqlInterpolated($"SELECT * FROM ACCOUNTS WHERE login = '{user}'");
+
+            // Sql Raw / ExecuteSqlRaw - Implicit Db Parameter
+            // SAFE
+            _context.Accounts
+                .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login = {0}", user);
+            // SAFE
+            _context.Database.ExecuteSqlRaw("SELECT * FROM ACCOUNTS WHERE login = {0}", user);
+
+            // Interpolated Sql - Implicit Db Parameter
+            // SAFE
+            _context.Accounts
+                .FromSqlInterpolated($"SELECT * FROM ACCOUNTS WHERE login = {user}");
+            // SAFE
+            _context.Database.ExecuteSqlInterpolated($"SELECT * FROM ACCOUNTS WHERE login = {user}");
+
+            _context.Accounts.FromSqlRaw($"SELECT * FROM ACCOUNTS WHERE login = '{user}'");
+
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable1(string user)
+        {
+            var concatSql = "SELECT * FROM ACCOUNTS WHERE login = '" + user + "'";
+            _context.Accounts
+                .FromSqlRaw(concatSql);
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable2(string user)
+        {
+             var concatSql = "SELECT * FROM ACCOUNTS WHERE login = '" + user + "'";
+            _context.Database.ExecuteSqlRaw(concatSql); ;
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable3(string user)
+        {
+            var formatStringSql = string.Format("SELECT * FROM ACCOUNTS WHERE login = '{0}'", user);
+            _context.Accounts
+                .FromSqlRaw(formatStringSql);
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable4(string user)
+        {
+            var formatStringSql = string.Format("SELECT * FROM ACCOUNTS WHERE login = '{0}'", user);
+            _context.Database.ExecuteSqlRaw(formatStringSql);
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable5(string user)
+        {
+            var interpolatedStringSql = $"SELECT * FROM ACCOUNTS WHERE login = '{user}'";
+            _context.Accounts
+                .FromSqlRaw(interpolatedStringSql);
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable6(string user)
+        {
+            var interpolatedStringSql = $"SELECT * FROM ACCOUNTS WHERE login = '{user}'";
+            _context.Database.ExecuteSqlRaw(interpolatedStringSql);
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable7(string user)
+        {
+            _context.Accounts
+                .FromSqlRaw($"SELECT * FROM ACCOUNTS WHERE login = '{user}'");
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreInjectionVulnerable8(string user)
+        {
+            var interpolatedStringSql = $"SELECT * FROM ACCOUNTS WHERE login = '{user}'";
+
+            _context.Database.ExecuteSqlRaw(interpolatedStringSql);
+            return new OkResult();
+        }
+
+    }
+}
---
+++ SharpSaster/SqlFlowController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharpSaster.Data;
+
+namespace SharpSaster
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlFlowController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SqlFlowController(DataContext context)
+        {
+            _context= context; 
+        }
+
+        public IActionResult EfCoreFlow1(string input, string otherparam)
+        {
+            DoStuffLevel1(input);
+
+            return new OkResult(); ;
+        }
+
+        private void DoStuffLevel1(string inputLevel1)
+        {
+            DoStuffLevel2(inputLevel1);
+        }
+
+        private void DoStuffLevel2(string inputLevel2)
+        {
+            var concatSql = "SELECT * FROM ACCOUNTS WHERE login = '" + inputLevel2 + "'";
+            _context.Accounts
+                .FromSqlRaw(concatSql);
+        }
+
+
+        public IActionResult EfCoreFlow2(string input, string otherparam)
+        {            
+            DoOtherStuffLevel1(input);            
+
+            DoStuffLevel1(input);
+
+            return new OkResult();
+        }
+
+        private void DoOtherStuffLevel1(string otherStuffLevel1)
+        {
+            string storingOtherStuff = "stored: " + otherStuffLevel1;
+        }
+
+
+        public IActionResult EfCoreFlow3(string input, string otherparam)
+        {
+            var otherVar = input + "tainted";
+
+            DoOtherStuffLevel1(otherVar);
+            DoStuffLevel1(otherVar);
+
+            return new OkResult();
+        }
+
+        public IActionResult EfCoreFlow4(string input, string otherparam)
+        {
+            var localCondition = true;
+
+            if (localCondition)
+            {
+                DoStuffLevel1(input);
+            }            
+
+            return new OkResult();
+        }
+
+
+
+        // UNREACHABLE
+        public IActionResult EfCoreFlow10(string input, string otherparam)
+        {
+            return new OkResult();
+
+            DoStuffLevel1(input);
+        }
+
+        // UNREACHABLE
+        public IActionResult EfCoreFlow11(string input, string otherparam)
+        {
+            var localCondition = false;
+
+            if (localCondition)
+            {
+                DoStuffLevel1(input);
+            }
+
+            return new OkResult();
+        }
+
+        // HARMLESS (rachable with null or empty input
+        public IActionResult EfCoreFlow12(string input, string otherparam)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                return new OkResult();
+            }
+
+            DoStuffLevel1(input);
+
+            return new OkResult();
+        }
+    }
+}
