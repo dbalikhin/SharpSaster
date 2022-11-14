336507e407b5b17e1aeeb237b2e7de1b526108d1
Dmitry <dmodeus@gmail.com>
2022-11-14 09:14:56 -0500

    safe code to build solution

---
+++ SharpSaster/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace SharpSaster.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        private IWebHostEnvironment _env;
+
+
+        public DataContext(DbContextOptions<DataContext> options, IWebHostEnvironment env)
+            : base(options)
+        {
+            _env = env;
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
