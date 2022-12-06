4bb173e740d9fcb3aa47d5381b60fcf0fe29ec21
Dmitry <dmitry@andliza.com>
2022-12-05 22:55:07 -0500

    interface

--- SharpSaster/Program.cs
+++ SharpSaster/Program.cs
@@ -1,7 +1,11 @@
+using SharpSaster;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddTransient<IRepo, Repo>();
 
 var app = builder.Build();
 
---
+++ SharpSaster/SqlBasicInterfaceController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SharpSaster.Data;
+using System.Collections;
+using System.Data;
+
+namespace SharpSaster
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlBasicInterfaceController : ControllerBase
+    {
+        public IRepo _repo1 = new Repo();
+
+        private readonly IRepo _repo2;
+
+        public SqlBasicInterfaceController(IRepo repo)
+        {            
+            _repo2 = repo;
+        }
+
+
+        public IActionResult SqlClientVulnerable1(string username, string name)
+        {
+            _repo1.DoRepoStuff(username, name);
+
+            return new OkResult();
+        }
+
+        public IActionResult SqlClientVulnerable2(string username, string name)
+        {
+            _repo2.DoRepoStuff(username, name);
+
+            return new OkResult();
+        }
+    }
+
+
+    public interface IRepo
+    {
+        void DoRepoStuff(string username, string name);
+    }
+
+    public class Repo : IRepo
+    {
+        public void DoRepoStuff(string username, string name)
+        {
+            string concatSql = "SELECT * FROM Accounts WHERE Username='" + username + "' OR  Name='" + name + "'";
+
+            using (SqlConnection connection = new SqlConnection("dummyconnectionstring"))
+            {
+                SqlCommand concatSqlCommand = new SqlCommand()
+                {
+                    CommandText = concatSql,
+                    CommandType = CommandType.Text,
+                };
+
+                concatSqlCommand.ExecuteReader();
+            }
+        }
+    }
+}
